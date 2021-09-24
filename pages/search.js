f89aabd7af2430231c9f08ba6659122e402f8018
moonihun <anslgns1@gmail.com>
2021-09-24 09:34:34 +0900

    day3

---
+++ pages/search.js
@@ -0,0 +1,69 @@
+import { format } from 'date-fns';
+import { useRouter } from 'next/dist/client/router';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import InfoCard from '../components/InfoCard';
+
+function Search({ searchResults }) {
+  const router = useRouter();
+  const { location, startDate, endDate, noOfGuests } = router.query;
+
+  const formattedStartDate = format(new Date(startDate), 'dd MMMM yy');
+  const formattedEndDate = format(new Date(endDate), 'dd MMMM yy');
+  const range = `${formattedStartDate} - ${formattedEndDate}`;
+
+  return (
+    <div>
+      <Header placeholder={`${location} | ${range} | ${noOfGuests}`} />
+      <main className='flex'>
+        <section className='flex-grow pt-14 px-6'>
+          <p className='text-xs'>
+            300개 이상의 숙소 · {range} · 게스트 {noOfGuests}명
+          </p>
+          <h1 className='text-3xl font-semibold mt-2 mb-4'>
+            {location}의 숙소
+          </h1>
+
+          <div className='hidden lg:inline-flex mb-5 space-x-3 text-gray-800 whitespace-nowrap'>
+            <p className='button'>취소 수수료 없음</p>
+            <p className='button'>숙소 유형</p>
+            <p className='button'>요금</p>
+            <p className='button'>침실과 침대</p>
+            <p className='button'>필터 추가하기</p>
+          </div>
+
+          <div className='flex flex-col'>
+            {searchResults?.map(
+              ({ img, location, title, description, star, price, total }) => (
+                <InfoCard
+                  img={img}
+                  location={location}
+                  title={title}
+                  description={description}
+                  star={star}
+                  price={price}
+                  total={total}
+                />
+              )
+            )}
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Search;
+
+export async function getServerSideProps() {
+  const searchResults = await fetch('https://links.papareact.com/isz').then(
+    (res) => res.json()
+  );
+
+  return {
+    props: {
+      searchResults,
+    },
+  };
+}
