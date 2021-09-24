1c961f7f42b55a66108a3cfee4eee0a997dfc10f
moonihun <anslgns1@gmail.com>
2021-09-24 11:40:18 +0900

    day4

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ['links.papareact.com'],
   },
+  env: {
+    mapbox_key:
+      'pk.eyJ1IjoibW9vbmlodW4iLCJhIjoiY2t0eHB2bXNxMHR4cTJ1bGVoOHhmOXVuOSJ9.MMvNzQF-ttQHFs9V5OYhGQ',
+  },
 };
